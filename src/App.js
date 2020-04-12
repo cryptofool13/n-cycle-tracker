d4bd33a65ee24b885160a70cf5c68c6584a00806
michaelcurry95 <fender18295@gmail.com>
2020-04-12 15:54:20 -0400

    cleaned up App

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 
 import Inputs from "./components/Inputs";
 import Chart from "./components/Chart";
-import { useLocalStorage } from "./utilites";
+import { useLocalStorage } from "./hooks";
 
 import dummyData from "./data";
 
 const App = () => {
-	const [data, setData] = useLocalStorage();
+	const [data, setData] = useLocalStorage("n-cycle-data", []);
+
 	return (
 		<>
 			<Inputs storage={[data, setData]} />
-			<Chart data={dummyData} />
+			<Chart data={data} />
 		</>
 	);
 };
