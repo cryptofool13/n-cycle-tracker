e4223255687cd14e92bcd8dc4babddb0f6ba7790
michaelcurry95 <fender18295@gmail.com>
2020-04-12 17:01:00 -0400

    pH chart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 
-import NitInputs from "./components/N2Inputs";
-import NitChart from "./components/charts/NitChart";
 import { useLocalStorage } from "./hooks";
+import NitInputs from "./components/N2Inputs";
 import PhInput from "./components/PhInput";
-
+import NitChart from "./components/charts/NitChart";
+import PhChart from "./components/charts/PhChart";
 // import dummyData from "./data";
 
 const App = () => {
 	const [nCycle, setNCycle] = useLocalStorage("n-cycle-data", []);
-	const [ph, setPh] = useLocalStorage("ph-data", [])
-	
+	const [ph, setPh] = useLocalStorage("ph-data", []);
+
 	return (
 		<>
 			<NitInputs storage={[nCycle, setNCycle]} />
 			<NitChart data={nCycle} />
 			<PhInput storage={[ph, setPh]} />
+			<PhChart data={ph} />
 		</>
 	);
 };
--- src/components/charts/PhChart.js
+++ src/components/charts/PhChart.js
@@ -1,1 +1,41 @@
 import React from "react";
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+} from "recharts";
+
+const PhChart = ({data}) => (
+	<>
+		<h3>pH</h3>
+		<LineChart
+			width={500}
+			height={200}
+			data={data}
+			margin={{
+				top: 5,
+				right: 30,
+				left: 20,
+				bottom: 5,
+			}}>
+			<CartesianGrid strokeDasharray="3 3" />
+			<XAxis dataKey="date" left={{ top: 20 }} />
+			<YAxis domain={[0,14]} interval={0} />
+			<Tooltip />
+			<Legend verticalAlign="top" />
+			<Line
+				type="monotone"
+				dataKey="ph"
+				stroke="#61b4ff"
+				activeDot={{ r: 4 }}
+			/>
+		</LineChart>
+    {console.log("data", data)}
+	</>
+);
+
+export default PhChart;
