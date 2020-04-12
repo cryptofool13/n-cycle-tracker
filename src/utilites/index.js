aa55239f5fcd817adec1dd67b4b1365727e0ebf6
michaelcurry95 <fender18295@gmail.com>
2020-04-12 15:51:17 -0400

    moment

--- src/utilites/index.js
+++ src/utilites/index.js
@@ -1,24 +1,7 @@
-import { useState, useEffect } from "react";
+import moment from "moment";
 
-export const today = () =>
-	new Date().toDateString().split(" ").slice(1).join(" ");
+export const today = () => moment().format("M-D");
 
 export const stringify = (data) => JSON.stringify(data);
 
 export const parse = (data) => JSON.parse(data);
-
-export const useLocalStorage = () => {
-	const [data, setData] = useState([]);
-
-	const getData = async () => {
-		let localData = parse(localStorage.getItem("n-cycle-data"));
-		if (!localData) return;
-		setData(localData);
-	};
-
-	useEffect(() => {
-		getData();
-	}, []);
-
-	return [data, setData];
-};
