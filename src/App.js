4a1a2a5cb988a35ef05c98ac83310aad5104418f
cryptofool13 <fender18295@gmail.com>
2020-04-25 15:17:54 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+// https://reacttraining.com/react-router/web/example/basic
+// https://reacttraining.com/react-router/web/example/auth-workflow
+
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { useLocalStorage } from "./hooks";
 import NitInputs from "./components/N2Inputs";
@@ -8,16 +12,21 @@ import PhChart from "./components/charts/PhChart";
 // import dummyData from "./data";
 
 const App = () => {
-	const [nCycle, setNCycle] = useLocalStorage("n-cycle-data", []);
-	const [ph, setPh] = useLocalStorage("ph-data", []);
+  const [nCycle, setNCycle] = useLocalStorage("n-cycle-data", []);
+  const [ph, setPh] = useLocalStorage("ph-data", []);
 
-	return (
-		<>
-			<NitInputs storage={[nCycle, setNCycle]} />
-			<NitChart data={nCycle} />
-			<PhInput storage={[ph, setPh]} />
-			<PhChart data={ph} />
-		</>
-	);
+  return (
+    <>
+      {/* <NitInputs storage={[nCycle, setNCycle]} />
+      <NitChart data={nCycle} />
+      <PhInput storage={[ph, setPh]} />
+      <PhChart data={ph} /> */}
+      <Router>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+    </>
+  );
 };
 export default App;
