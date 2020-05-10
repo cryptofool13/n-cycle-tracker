71acee87cef1f3e47d5998bca232a0641ca7fee3
cryptofool13 <fender18295@gmail.com>
2020-05-09 20:56:06 -0400

    home page and style

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import './styles/App.css'
 import { useLocalStorage } from "./hooks";
-import NitInputs from "./components/N2Inputs";
-import PhInput from "./components/PhInput";
-import NitChart from "./components/charts/NitChart";
-import PhChart from "./components/charts/PhChart";
+import Home from './components/pages/Home'
+// import NitInputs from "./components/N2Inputs";
+// import PhInput from "./components/PhInput";
+// import NitChart from "./components/charts/NitChart";
+// import PhChart from "./components/charts/PhChart";
 // import dummyData from "./data";
 
 const App = () => {
@@ -21,9 +23,15 @@ const App = () => {
       <NitChart data={nCycle} />
       <PhInput storage={[ph, setPh]} />
       <PhChart data={ph} /> */}
+      {/* Nav bar/LogIN-Logout */}
       <Router>
         <Switch>
-          <Route path="/"></Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            {/* dashboard page, requireAuth */}
+          </Route>
         </Switch>
       </Router>
     </>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <header className="home">
+      <h1 className="home-header">NITRO-GRAPH</h1>
+      <div className="authBtns-home">
+        <button className="authBtn-home">LOG IN</button>
+        <button className="authBtn-home">CREATE ACCOUNT</button>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
